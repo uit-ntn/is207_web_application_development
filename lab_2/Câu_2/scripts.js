5b9a82df58fd4110aaa3ab463d2b52e4a8a1f1f2
uit-ntn <npthanhnhan2003@gmail.com>
2023-10-19 21:24:48 +0700

    done cau1, cau2, cau4 ,cau5, cau6

--- lab_2/Câu_2/scripts.js
+++ lab_2/Câu_2/scripts.js
@@ -1,17 +1,43 @@
-document.querySelector(".calculate-bill-btn").onclick = (e) => {
-    e.preventDefault();
+document.querySelector('.calculate-bill-btn').onclick = (event) => {
+    event.preventDefault();
+    const selectedFood = document.querySelector('select[name="food"]').selectedOptions; 
+    // const selectedFood = document.querySelector('select[name="food"]] option:checked')
+    const selectDrink = document.querySelector('select[name="drink"]').selectedOptions;
+    const timeInput = document.querySelector('input[name="time"]:checked');
+    const billInfo = document.querySelector('.bill-info-container');
 
-    const foodSelect = document.querySelector('select[name="food"]');
-    const drinkSelect = document.querySelector('select[name="drink"]');
-    const timeRadio = document.querySelector('input[name="time"]:checked');
+    if (!selectedFood || !selectDrink || !timeInput) {
+        alert('Nhập đầy đủ thông tin');
+    } else {
+        const selected = [...selectedFood, ...selectDrink];
 
-    const selectItems = [...foodSelect.options, ...drinkSelect.options];
-    for (item in selectItems) {
-
+        billInfo.innerHTML = `
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Các món đã dùng</th>
+                        <th>Tiền</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${selected.map(option => `
+                        <tr>
+                            <td>${option.textContent}</td>
+                            <td>${option.value}</td>
+                        </tr>
+                    `).join('')}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td>Tổng tiền</td>
+                        <td>${Math.round(
+            (selected.reduce((totalPrice, option) => totalPrice + parseInt(option.value), 0)
+                * (timeInput.value === "ban_ngay" ? 1 : 1.1
+                )))} đồng</td>
+                    </tr>
+                </tfoot>
+            </table>
+        `;
+        billInfo.style.display = "block";
     }
-    console.log(foodSelect);
-    console.log(drinkSelect);
-    console.log(timeRadio);
-    console.log(selectItems);
-
-};
+}
