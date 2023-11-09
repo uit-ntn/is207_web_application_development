e0c71f9fc992306b11bded9b4d27ffba9de94a2a
Nhan <npthanhnhan2003@gmail.com>
2023-11-09 09:01:56 +0700

    add jquery-cau-1

--- lab_2/Câu_1/scripts.js
+++ lab_2/Câu_1/scripts.js
@@ -1,15 +1,27 @@
-const calculateSalary = () => {
+// const calculateSalary = () => {
+//     event.preventDefault();
+//     const salary = parseFloat(document.querySelector('.salary-input').value);
+//     const coefficient = parseFloat(document.querySelector('.coefficient-input').value);
+//     const income = document.querySelector('.income');
+
+//     if (!isNaN(salary) && !isNaN(coefficient)) {
+//       income.textContent = `${salary * coefficient}`;
+//     } else {
+//       income.textContent = "nhập giá trị hợp lệ";
+//     }
+//   }
+//   document.querySelector('.calculate-salary-btn').addEventListener('click', calculateSalary);
+
+$(document).ready(() => {
+
+  $(".calculate-salary-btn").on('click',() => {
     event.preventDefault();
-    const salary = parseFloat(document.querySelector('.salary-input').value);
-    const coefficient = parseFloat(document.querySelector('.coefficient-input').value);
-    const income = document.querySelector('.income');
-
-    if (!isNaN(salary) && !isNaN(coefficient)) {
-      income.textContent = `${salary * coefficient}`;
-    } else {
-      income.textContent = "nhập giá trị hợp lệ";
-    }
-  }
-  document.querySelector('.calculate-salary-btn').addEventListener('click', calculateSalary);
-
-
+    const salary = $('.salary-input').val();
+    const coefficient = $('.coefficient-input').val();
+    const income = $('.income');
+    if (!isNaN(salary) && !isNaN(coefficient))
+      income.text(`${salary * coefficient}`)
+    else
+      income.text("nhập giá trị hợp lệ");
+  });
+})
